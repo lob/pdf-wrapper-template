de8c24feb54deb56fd990b296d0494eeeac68e34
Nick Perri <nick.perri@lob.com>
2023-03-20 11:11:32 -0500

    clean up jsdoc

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.renderPDF = void 0;
 /**
  *
- * @param pathToPDFFile The path to a pdf file
- * @param fileInfo A binary represenation of the attributes required to render the PDF.
+ * @param {string} pathToPDFFile The path to a pdf file
+ * @param {number} fileInfo - A binary represenation of the attributes required to render the PDF.
  * You can choose which LSB represents which characteristic (annotation, encryption, search)
- * @returns
+ * @returns {PDFWrapper}
  */
 function renderPDF(pathToPDFFile, fileInfo) {
     // Implementation goes here. You can leave it in this file or move it elsewhere
--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,10 @@ interface PDFWrapper {
 
 /**
  * 
- * @param pathToPDFFile The path to a pdf file
- * @param fileInfo A binary represenation of the attributes required to render the PDF.
+ * @param {string} pathToPDFFile - The path to a pdf file
+ * @param {number} fileInfo - A binary represenation of the attributes required to render the PDF.
  * You can choose which LSB represents which characteristic (annotation, encryption, search)
- * @returns 
+ * @returns {PDFWrapper} - A PDFWrapper that wraps a selected PDF rendering engine
  */
 export function renderPDF(pathToPDFFile: string, fileInfo: number): PDFWrapper {
   // Implementation goes here. You can leave it in this file or move it elsewhere
