fb0a2d8f767710fb3a51ba410b38c621412b0ace
Nick Perri <nick.perri@lob.com>
2023-04-04 08:45:29 -0500

    updates for clarity

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,12 +2,14 @@ import { renderPDF } from '../src/index';
 
 /**
  * Add additional unit tests here to ensure your code works properly for all use cases.
- * Feel free to adjust the structure of tests as suites your needs
+ * Feel free to adjust the structure of tests as suites your needs.
+ * 
+ * You do NOT need to test actually opening or manipulating a PDF document. Testing mock responses is perfect
  */
 describe('renderPDF', () => {
   
   it('chooses BestPDFEngine if the file requires Annotation, Encryption and Search', () => {
-    const fileInfoByte = 111;
+    const fileInfoByte = 0b111;
     const pathToPDFFile = './example.pdf';
     const pdfWrapper = renderPDF(pathToPDFFile, fileInfoByte);
     expect(pdfWrapper.engineName).toEqual('BestPDFEngine');
